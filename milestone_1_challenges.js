f6692b503c1ba1c02a77fc299b90142395ed2855
Halima <natashawanjiku31@gmail.com>
2025-05-20 18:41:56 -0400

    Add my answers from question 5-10

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -48,3 +48,98 @@ function canDrive(name,age){
 }
 canDrive("Antony" , 10)
 canDrive("Tasha", 30)
+
+//Largest Number
+
+function findLargest(a, b, c){
+    if(a>b && a>c){
+        console.log (`a is greater than b and c`)
+    } else if(b>a && b>c){
+        console.log(`b is greater than a and c`)
+    }else{
+        console.log(`c is greater than a and b`)
+    }
+}
+
+findLargest(5,6,8)
+findLargest(5,6,-8)
+findLargest(5,-6,-8)
+
+//BMI Calculator
+function calculateBMI(weight, height){
+    let BMI = weight / (height * height)
+
+    if (BMI<18.5){
+        console.log(`Your BMI is ${BMI} - Under weight`)
+    }else if(BMI>=18.5 && BMI<=24.9){
+        console.log(`Your BMI is ${BMI} - Normal weight`)
+    }else if(BMI>=25 && BMI<=29.9){
+        console.log(`Your BMI is ${BMI} - Over weight`)
+    }else{
+        console.log(`Your BMI is ${BMI} - Obese`)
+    }
+}
+calculateBMI(30, 1.60)
+calculateBMI(60, 1.60)
+calculateBMI(70, 1.60)
+calculateBMI(80, 1.60)
+
+//Greeting Based On Time
+
+function greetUser(name, hour){
+    if(hour>=5 && hour <= 11){
+        console.log(`Good morning, ${name}`)
+    }else if(hour>=12 && hour <= 17){
+        console.log(`Good afternoon, ${name}`)
+    }else if(hour>=18 && hour<=21){
+        console.log(`Good evening, ${name}`)
+    }else {
+        console.log(`Good night, ${name}`)
+    }
+}
+
+greetUser("Tasha", 6)
+greetUser("Tasha", 15)
+greetUser("Tasha", 20)
+greetUser("Tasha", 23)
+
+function fizzBuzzCheck(number){
+    if(number % 3 == 0){
+        if (number % 5 == 0){
+        console.log(`FizzBuzz`)
+    }
+    else if(number % 3 == 0){
+        console.log(`Fizz`)
+    }
+
+    }
+    // else if(number % 3 == 0){
+    //     console.log(`Fizz`)
+    // }
+    else if(number % 5 == 0){
+        console.log(`Buzz`)
+    }else {
+        console.log(`${number}`)
+    }
+}
+
+fizzBuzzCheck(15)
+fizzBuzzCheck(9)
+fizzBuzzCheck(10)
+fizzBuzzCheck(22)
+
+//Perimeter 2
+function perimeterAndCircle(l,num){
+    if(l=="s"){
+        console.log((num*4))
+    }else if(l=="c"){
+        console.log((6.28*num))
+    }
+    else{
+        console.log(`invalid`)
+    }
+}
+
+perimeterAndCircle("s",4)
+perimeterAndCircle("c",4)
+perimeterAndCircle("p",4)
